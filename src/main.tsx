e86202264ca06f8cbf1aa7b72abea325bb605ac0
sumonpaul55 <sumonpaul.web@gmail.com>
2025-12-24 14:31:14 +0600

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { MainLayout } from './layouts/MainLayout'
+import { Aboutus } from './pages/about/Aboutus'
+import { Contact } from './pages/contact/Contact'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route index element={<MainLayout />} />
         <Route path="/" element={<MainLayout />} />
+        <Route path="about" element={<Aboutus />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>,
   </StrictMode>,
