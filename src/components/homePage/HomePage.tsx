3ade2f112bc007d06b9f39976a6f9b1d1772fc50
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2026-05-14 23:12:37 +0600

    Project showcase done

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import FeaturedProject from "./FeaturedProject";
 import ProcessWeFollow from "./ProcessWeFollow";
+import ProjectShowcase from "./ProjectShowcase";
 import Service from "./Service";
 import WhyPartner from "./WhyPartner";
 
 export default function HomePage() {
   return (
     <div className="bg-primary">
+      <ProjectShowcase/>
       <Service />
       <ProcessWeFollow />
       <WhyPartner />
